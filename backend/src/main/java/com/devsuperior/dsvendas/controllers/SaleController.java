23fccaa2398e349cf781cc3f168a7655a45569a8
Ederson Philereno <ederphil@gmail.com>
2021-05-29 01:44:49 -0300

    Group by search

--- backend/src/main/java/com/devsuperior/dsvendas/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsvendas/controllers/SaleController.java
@@ -1,5 +1,7 @@
 package com.devsuperior.dsvendas.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dsvendas.dto.SaleDto;
+import com.devsuperior.dsvendas.dto.SaleSumDto;
+import com.devsuperior.dsvendas.dto.SaleSuccessDto;
 import com.devsuperior.dsvendas.services.SaleService;
 
 @RestController
@@ -22,6 +26,22 @@ public class SaleController {
 	public ResponseEntity<Page<SaleDto>> findAll(Pageable e) {
 
 		return ResponseEntity.ok(saleService.findAll(e));
+
+	}
+
+	@GetMapping(value = "/amount-by-seller")
+	public ResponseEntity<List<SaleSumDto>> amountGroupedBySeller() {
+
+		List<SaleSumDto> list = saleService.amountGroupedBySeller();
+		return ResponseEntity.ok(list);
+
+	}
+
+	@GetMapping(value = "/success-by-seller")
+	public ResponseEntity<List<SaleSuccessDto>> successGroupedBySeller() {
+
+		List<SaleSuccessDto> list = saleService.successGroupedBySeller();
+		return ResponseEntity.ok(list);
 
 	}
 
---
+++ backend/src/main/java/com/devsuperior/dsvendas/dto/SaleSumDto.java
@@ -0,0 +1,39 @@
+package com.devsuperior.dsvendas.dto;
+
+import java.io.Serializable;
+
+import com.devsuperior.dsvendas.entities.Seller;
+
+public class SaleSumDto implements Serializable{
+
+	private static final long serialVersionUID = -8863452869400247972L;
+	
+	private String sellerName;
+	private Double sum;
+	
+	public SaleSumDto() {
+		
+	}
+	
+	public SaleSumDto(Seller seller, Double sum) {
+		this.sellerName = seller.getName();
+		this.sum = sum;
+	}
+
+	public String getSellerName() {
+		return sellerName;
+	}
+
+	public void setSellerName(String sellerName) {
+		this.sellerName = sellerName;
+	}
+
+	public Double getSum() {
+		return sum;
+	}
+
+	public void setSum(Double sum) {
+		this.sum = sum;
+	}
+	
+}
